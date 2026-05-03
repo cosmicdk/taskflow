4c85e5b70c7795b1ad5d927bcc2b65061a79d9d0
lan <46103901+cosmicdk@users.noreply.github.com>
2026-05-03 19:32:31 +0800

    📋 CategoryDTO

---
+++ taskflow-web/src/main/java/com/taskflow/dto/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.taskflow.dto;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+    @NotBlank(message = "分类名称不能为空")
+    private String name;
+    private String color;
+    private Integer sortOrder;
+}
