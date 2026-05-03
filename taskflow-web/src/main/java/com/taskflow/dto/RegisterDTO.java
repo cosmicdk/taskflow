1d8af809e020012bd7767e38fb14d7cc9e9f9456
lan <46103901+cosmicdk@users.noreply.github.com>
2026-05-03 19:31:44 +0800

    📋 RegisterDTO

---
+++ taskflow-web/src/main/java/com/taskflow/dto/RegisterDTO.java
@@ -0,0 +1,19 @@
+package com.taskflow.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterDTO {
+    @NotBlank(message = "用户名不能为空")
+    @Size(min = 3, max = 20, message = "用户名长度应在3-20之间")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 6, max = 30, message = "密码长度应在6-30之间")
+    private String password;
+
+    private String nickname;
+    private String email;
+}
